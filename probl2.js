0a4cddae9e5ebc7a1c6259c53b8f7c054036eb4a
Simina Birzava <simi.birzava25@gmail.com>
2023-02-06 12:00:59 +0100

    index.html

---
+++ probl2.js
@@ -0,0 +1,10 @@
+const x = 5;
+const y = 8;
+// Exercitul 1
+
+const sum1 = 2 * x - 5 * y;
+console.log(sum1);
+
+// Exercitiul 2
+const sum2 = 3 * x + 9 * y;
+console.log(sum2);
---
+++ script.js
@@ -0,0 +1,28 @@
+// Exercitiul 1
+
+const sum1 = 2 + 3 * 4 - 5;
+
+console.log(sum1);
+
+// Exercitiul 2
+
+const sum2 = 4 * (2 + 6 / 3);
+
+console.log(sum2);
+
+// Exercitiul 3
+
+const sum3 = 2 * 3 ** 2 + 2 ** 5;
+
+console.log(sum3);
+
+// Exercitiul 4
+const sum4 = (5 * (3 + 2 / 2)) + 3;
+
+console.log(sum4);
+
+// Exercitiul 5
+
+const sum5 = 2 ** 5 - 5 ** 2 + 3 * (5 - 4 / 2);
+const sum6 = 2 ** 5 + 5 ** 2;
+console.log(sum5 / sum6);
